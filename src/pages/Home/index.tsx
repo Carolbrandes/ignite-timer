bb929ec3a6cad39fe7ce1327c25d83322d573499
carol <carolina.brandes@corebiz.ag>
2023-12-14 07:07:29 -0300

    home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,24 +7,40 @@ export function Home() {
       <form action="">
         <S.FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+          <S.TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          {/* sugestao para inputs */}
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+          </datalist>
 
           <label htmlFor="minutesAmount">Vou trabalhar em</label>
-          <input id="minutesAmount" type="number" />
+          <S.MinutesAmountInput
+            id="minutesAmount"
+            type="number"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
           <span>minutos.</span>
         </S.FormContainer>
 
         <S.CountdownContainer>
           <span>0</span>
           <span>0</span>
-          <span>:</span>
+          <S.Separator>:</S.Separator>
           <span>0</span>
           <span>0</span>
         </S.CountdownContainer>
 
-        <button type="submit">
+        <S.StartCountdownButton type="submit">
           <Play size={24} /> Começar
-        </button>
+        </S.StartCountdownButton>
       </form>
     </S.HomeContainer>
   )
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
     *{
@@ -9,15 +9,16 @@ export const GlobalStyle = createGlobalStyle`
 
     :focus{
         outline: 0;
-        box-shadow: 0 0 0 2px ${props => props.theme["gray-500"]};
+        box-shadow: 0 0 0 2px ${(props) => props.theme['gray-500']};
     }
 
     body{
-        background: ${props => props.theme["gray-900"]};
-        color: ${props => props.theme["gray-300"]};
+        background: ${(props) => props.theme['gray-900']};
+        color: ${(props) => props.theme['gray-300']};
+        -webkit-font-smoothing: antialiased;
     }
 
-    border-style, input-security, textarea, button {
+    body, input, textarea, button {
         font-family: 'Roboto', sans-serif;
         font-weight: 400;
         font-size: 1rem;
