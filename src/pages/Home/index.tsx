7dd5d6f271561bfcf0703f8f716bd540158df7f7
carol <carolina.brandes@corebiz.ag>
2023-12-27 19:18:18 -0300

    Home: add the cycle and show the time in screen

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,29 +3,60 @@ import * as S from './styles'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
+import { useState } from 'react'
+
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+}
 
 export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
+
+
   const newCycleFormValidationSchema = zod.object({
     task: zod.string().min(1, 'informe a tarefa'),
     minutesAmount: zod.number().min(5, 'O ciclo precisa ser de no mínimo 5 minutos').max(60, 'O ciclo precisa ser de no máximo 60 minutos')
   })
 
-type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema> //*typeof - referenciar uma variavel no ts
+  type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema> //*typeof - referenciar uma variavel no ts
 
- const {register, handleSubmit, watch, reset} = useForm<NewCycleFormData>({
-  resolver: zodResolver(newCycleFormValidationSchema),
-  defaultValues: {
-    task: '',
-    minutesAmount: 0
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0
+    }
+  })
+
+  function handleCreateNewCycle({ task, minutesAmount }: NewCycleFormData) {
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      task,
+      minutesAmount
+    }
+
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
+    reset()
   }
- })
 
- function handleCreateNewCycle(data: NewCycleFormData) {
-  reset()
- }
-
- const task = watch('task') // *watch - controlled component
- const isSubmitDisabled = !task
+  const task = watch('task') // *watch - controlled component
+  const isSubmitDisabled = !task
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+  const minutesAmount = Math.floor(currentSeconds / 60)
+  const secondsAmount = currentSeconds % 60
+  const minutes = String(minutesAmount).padStart(2, '0')
+  const seconds = String(secondsAmount).padStart(2, '0')
+  
 
   return (
     <S.HomeContainer>
@@ -52,17 +83,17 @@ type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema> //*typeof
             step={5}
             min={5}
             max={60}
-             {...register('minutesAmount', {valueAsNumber: true})}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
           <span>minutos.</span>
         </S.FormContainer>
 
         <S.CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <S.Separator>:</S.Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </S.CountdownContainer>
 
         <S.StartCountdownButton type="submit" disabled={isSubmitDisabled}>
